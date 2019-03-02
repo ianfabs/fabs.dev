5f1ace7f420c23a838ba629f9b89baa84568e1f0
Ian Fabs <ianfabs@outlook.com>
2019-03-02 13:44:52 -0500

    Updated examples

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import Anime from 'react-anime';
 import {Terminal, Ty} from "./components/Terminal/";
+import JType from "./components/JType/";
+
+const typingName = <JType
+name="Ian Fabs"
+fps={40}
+caps={true}
+done={()=>{alert('done')}}
+/>;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Terminal>
+        {/* <Terminal>
           <Ty type="input">npm i -g termynal</Ty>
-          <Ty type="progress"></Ty>
+          <Ty type="progress"/>
           <Ty>Success</Ty>
-          <Ty>Tell me</Ty>
-        </Terminal>
+        </Terminal> */}
         {/* <Anime 
           delay={(e, i) => i * 100}
           scale={[0.1, 0.9]}
@@ -21,6 +28,9 @@ class App extends Component {
         >
           <div className="blue"/>
         </Anime> */}
+        <h1>
+          {typingName}
+        </h1>
       </div>
     );
   }
