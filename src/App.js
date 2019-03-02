85311c5852db8176c92058670fb7c264aadb3009
Ian Fabs <ianfabs@outlook.com>
2019-03-02 17:50:54 -0500

    Rewrote landing page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react';
+import styled from "styled-components";
 import './App.css';
-import Anime from 'react-anime';
-import {Terminal, Ty} from "./components/Terminal/";
-import JType from "./components/JType/";
-
-const typingName = <JType
-name="Ian Fabs"
-fps={40}
-func={()=>{alert('hello')}}
-/>;
+import {Window, Terminal, Line} from "./components/Terminal/";
+import JType from "jatrix-react/build/index";
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Terminal>
-          <Ty type="input">npm i -g termynal</Ty>
-          <Ty type="progress"/>
-          <Ty>Success</Ty>
-        </Terminal> */}
-        {/* <Anime 
-          delay={(e, i) => i * 100}
-          scale={[0.1, 0.9]}
-          loop={true}
-          direction="alternate"
-        >
-          <div className="blue"/>
-        </Anime> */}
-        <h1>
-          {typingName}
-        </h1>
+        <Box>
+          <h1>
+            <JType fps={10}>Hi, I'm ian fabs</JType>
+          </h1>
+          <h2>There's not much to see here yet, but I promise there will be soon!</h2>
+          <hr style={{width: "30vw", margin: "10px 0"}}/>
+          <h3>In the meantime, try installing some of my npm packages!</h3>
+          <Window style={{width: "50vw"}}>
+            <Terminal>
+              <Line type="input">npm i --save sleyp jatrix-react rholor jraph</Line>
+            </Terminal>
+          </Window>
+        </Box>
       </div>
     );
   }
